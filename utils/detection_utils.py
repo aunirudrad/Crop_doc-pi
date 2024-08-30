21bac742664c57f1cc169686237aeea0a0806e1a
Aunirudra Dey Anu <102872213+aunirudrad@users.noreply.github.com>
2024-08-31 04:42:01 +0600

    Update detection_utils.py

--- utils/detection_utils.py
+++ utils/detection_utils.py
@@ -3,15 +3,14 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 import streamlit as st
-import requests
+
 
 # Load the TFLite models
 model_paths = {
-    'mango': 'https://drive.google.com/uc?export=download&id=1Ag9othRVtRukNEpLZdH4MVdr6gK1tvVT',
-    'soybean': 'https://drive.google.com/uc?export=download&id=1h904aqG4kvafUDU-OjsPCizZE-ZfWajo',
-    'potato': 'https://drive.google.com/uc?export=download&id=1Q3ipq0ywKlUfNK6Ku5-uXj_34ksZxa1a'
+    'mango': 'mango_model.tflite',
+    'soybean': 'soybean_model.tflite',
+    'potato': 'potato_model.tflite'
 }
-response = requests.get(model_paths)
 
 interpreters = {}
 for crop, path in model_paths.items():
