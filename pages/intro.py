6fbf9477ee20c1f584c052bc1b2a1a3f982611cd
Aunirudra Dey Anu <102872213+aunirudrad@users.noreply.github.com>
2024-08-31 05:36:42 +0600

    Update intro.py

--- pages/intro.py
+++ pages/intro.py
@@ -3,7 +3,7 @@ import streamlit as st
 def intro_page(navigate):
     st.title("Welcome to Hawkeye-Eagle")
 
-    st.image("images\logo.png", width=400)
+    st.image("logo.png", width=400)
     
     # Add Start button
     if st.button("Start"):
