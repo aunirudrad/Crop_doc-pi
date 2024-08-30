0ddecdd8c6fc2c0457335a76cbeab08616ab4911
Aunirudra Dey Anu <102872213+aunirudrad@users.noreply.github.com>
2024-08-31 05:22:52 +0600

    Update intro.py

--- pages/intro.py
+++ pages/intro.py
@@ -2,14 +2,8 @@ import streamlit as st
 
 def intro_page(navigate):
     st.title("Welcome to Hawkeye-Eagle")
-    def display_image():
-        st.title("Image Display Example")
-        
-        # Load and display image
-        st.image("E:\Budget_ashle_fatai_dibo\hawkeye-agriculture\images\logo-removebg-preview.png", use_column_width=True)
-        
-        # Display image with specific width
-        st.image("E:\Budget_ashle_fatai_dibo\hawkeye-agriculture\images\logo-removebg-preview.png", width=400)
+
+    st.image("images\logo.png", width=400)
     
     # Add Start button
     if st.button("Start"):
